94c2e98ac8a2b30861fad3dd1efe908c6382342f
Augustin0716 <augustin.sathicq@gmail.com>
2025-09-07 01:09:01 +0200

    Made huge improvements :  - imported only Graphics from java.awt (the rest was useless)  - made it holds 2 labels  - made it detect the pause button to close itself, but it might need some further improvement about this

--- src/game/menu/PauseMenu.java
+++ src/game/menu/PauseMenu.java
@@ -3,32 +3,31 @@ package game.menu;
 import game.keyHandling.InputHandler;
 import game.keyHandling.GameActions;
 import game.menu.menuComponent.MenuComponent;
+import game.menu.menuComponent.Label;
 
-import java.awt.*;
+import java.awt.Graphics;
 
 public class PauseMenu<E extends Enum<E>> extends Menu {
 
-    public PauseMenu(MenuMaster<E> master, InputHandler input) {
-        super(master, input,0);
+
+    public PauseMenu(MenuMaster<E> master, InputHandler<GameActions> input) {
+        super(master, input,2);
         initComponents();
     }
 
     @Override
     public void initComponents() {
-
+        menuComponents[0] = new Label(this, "GAME PAUSED");
+        menuComponents[1] = new Label(this, "Press Del, F or 7 on the num pad to continue");
+        menuComponents[0].setPos(350, 200);
+        menuComponents[1].setPos(250, 235);
     }
 
     @Override
     public void update() {
-        if (checkCooldown()) {
-            if (input.actionActivated(GameActions.PAUSE)) {
-                master.closeMenu();
-                return;
-            }
-            if (input.actionActivated(GameActions.MENU_MOVE_DOWN)) {
-
-            }
-        }
+        if(input.actionActivated(GameActions.PAUSE)) {
+            master.closeMenu();
+        } //TODO : secure the menu so it doesn't close immediately
     }
 
     @Override
