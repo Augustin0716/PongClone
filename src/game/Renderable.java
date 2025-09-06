373f4bf7ed99cd5baf0816d59f5e0d0a99abc730
Augustin0716 <augustin.sathicq@gmail.com>
2025-09-06 19:22:59 +0200

    Added doc strings

--- src/game/Renderable.java
+++ src/game/Renderable.java
@@ -2,6 +2,44 @@ package game;
 
 import java.awt.Graphics;
 
+/**
+ * This functional interface should be used on objects that should be rendered on an AWT component. It takes as
+ * argument the Graphics object from said AWT component. The way it should be implemented is : if a class is an AWT
+ * component subclass (say a Canvas subclass) implementing Renderable, <code>render(Graphics g)</code> should generate
+ * g (while ignoring the parameter Graphics) and pass it down the objects it holds. Let's take for instance a Game
+ * object that holds a MatchManager object that itself holds a Ball object and 2 Racket objects, the way
+ * <code>render(Graphics g)</code> should be used is :
+ * <pre>{@code
+ * // Game extends Canvas
+ * public void render(Graphics unused) { // the parameter is not used
+ *     bs = getBufferStrategy();
+ *     Graphics g = bs.getDrawGraphics();
+ *     // the Graphics object is generated
+ *     matchManager.render(g);
+ *     g.dispose();
+ *     bs.show();
+ * }
+ * // MatchManager
+ * public void render(Graphics g) {
+ *     g.drawImage(backGround, 0, 0, xMax, yMax);
+ *     // the earlier the draw, the further back it is, so we start with the background
+ *     ball.render(g);
+ *     racket1.render(g);
+ *     racket2.render(g);
+ *     // then the objects are drawn, on the foreground
+ * }
+ * // Ball
+ * public void render(Graphics g) {
+ *     g.setColor(Color.WHITE); // don't forget about colors
+ *     g.fillOval(x - RADIUS, y - RADIUS, x + RADIUS, y + RADIUS);
+ *     // since it doesn't hold any object that should be rendered, Ball just render itself (a circle)
+ * }
+ * // Racket
+ * public void render(Graphics g) {
+ *     g.setColor(Color.WHITE);
+ *     g.fillRect(x, y, x + WIDTH, y + HEIGHT);
+ * }}</pre>
+ */
 @FunctionalInterface
 public interface Renderable {
     void render(Graphics g);
--- src/game/Updatable.java
+++ src/game/Updatable.java
@@ -1,9 +1,24 @@
 package game;
 
 /**
- * A functionnal interface for the method <code>void update()</code>. It doesn't take any argument so the class must get everything it needs to update itself, and doesn't return anything either.
- * To use this method efficiently, it should be called from high responsibility objects to lower responsibility objects, with the highest responsibility object run in a Thread.
- * For instance <code>MainObject implements Updatable</code> should call in its own <code>update()</code> method the <code>InnerObject implements Updatable</code>'s <code>update()</code> method, and so on.
+ * A functionnal interface for the method <code>void update()</code>. It doesn't take any argument so the class must get
+ * everything it needs to update itself, and doesn't return anything either. To use this method efficiently, it should
+ * be called from high responsibility objects to lower responsibility objects, with the highest responsibility object
+ * run in a Thread. For instance <code>MainObject implements Updatable</code> should call in its own <code>update()</code>
+ * method the <code>InnerObject implements Updatable</code>'s <code>update()</code> method, and so on as such :
+ * <pre>{@code
+ * // MainObject implements Runnable
+ * public void update() { //called from a main loop in a thread
+ *     this.changeStuff(); // the mainObject updates something
+ *     innerObject.update();
+ *     // the place of the line above might influence the update of innerObject, therefore it's something to consider
+ *     this.changeOtherStuff(); // the mainObject updates something else
+ * }
+ * // InnerObject
+ * public void update() {
+ *     this.moveStuff();
+ *     // since it doesn't hold any object that must be updated, it just update itself
+ * }}</pre>
  *
  */
 @FunctionalInterface
