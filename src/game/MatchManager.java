971263acddf27d36712941f32ec96f97cadfb25d
Augustin0716 <augustin.sathicq@gmail.com>
2025-09-07 01:17:20 +0200

    Made huge improvements :  - made some field private final, since they don't need to be public  - replaced servCountDown and scoreCountDown by countdown, a more generic int for all countdowns (useless to have more than 1)  - created field scoreSide, used to determine which player got the last point and/or won (so no need to modify testWin() anymore, unless we want to specify the tests)  - changed actionJustPressed() by actionActivated() to open the pause menu, which now works  - incorporated the WonMenu  - deleted renderBackGround since backGround.render(g) can be called directly  - minor changes that doesn't affect the dynamic of the game nor the performances

--- src/game/MatchManager.java
+++ src/game/MatchManager.java
@@ -4,10 +4,7 @@ import java.awt.Graphics;
 
 import game.keyHandling.GameActions;
 import game.keyHandling.InputHandler;
-import game.menu.BackGroundMenu;
-import game.menu.Menu;
-import game.menu.MenuMaster;
-import game.menu.PauseMenu;
+import game.menu.*;
 
 public class MatchManager implements Renderable, Updatable, MenuMaster<MatchManager.PauseMenuOptions> {
 
@@ -17,16 +14,16 @@ public class MatchManager implements Renderable, Updatable, MenuMaster<MatchMana
         SOUND_ON, SOUND_OFF
 
     }
-    public Game master;
+    private final Game master;
     private Menu menu;
-    public int middleX;
-    public int middleY;
-    public int gameMode;
+    private final int middleX;
+    private final int middleY;
+    private int gameMode;
+    private int scoreSide = 1;
     public Racket player1 = new Racket(1); //TODO : players should be left or right rather than 1 and 2
     public Racket player2 = new Racket(-1);
     public Ball ball = new Ball(this, player1, player2);
-    private int serveCountDown = 0;
-    private int scoreCountDown = 0;
+    private int countdown = 0;
     public int gameState = -1;
     private int scorePlayer1;
     private int scorePlayer2;
@@ -35,52 +32,66 @@ public class MatchManager implements Renderable, Updatable, MenuMaster<MatchMana
 
     public MatchManager(Game master, InputHandler<GameActions> input) {
         this.master = master;
+        this.input = input;
         this.middleX = Game.WIDTH / 2;
         this.middleY = Game.HEIGHT / 2;
         this.player1.x = Ball.RADIUS * 3;
         this.player2.x = Game.WIDTH - Ball.RADIUS * 3;
         resetPos();
-        this.input = input;
     }
 
     @Override
     public void update() {
         switch (gameState) {
             case -1 -> {
-                return; // in this case, the game hasn't been initialized, so there is nothing to update
+                // in this case, the game hasn't been initialized, so there is nothing to update
             }
 
             case 0 -> { // before the serve
-                if (serveCountDown > 0) {
-                    serveCountDown--;
+                if (countdown > 0) {
+                    countdown--;
                 } else {
-                    ball.speed.set((float) .2, 0);
+                    ball.speed.set(.2f * scoreSide, 0); // the ball is headed to the player that lost last point
                     gameState++;
                     for (Racket player : new Racket[] {player1, player2}) {
                         if (player instanceof ComputerPlayer) ((ComputerPlayer) player).setTargetY(ball);
                     }
                 }
             }
+
             case 1 -> { // the part where the game is actually played
                 gameLogic();
-                int goal = ball.touchDown();
-                if (goal == 0) return;
-                int score = (goal == 1)? ++scorePlayer1:++scorePlayer2;
-                backGround.updateScore(score, goal);
-                scoreCountDown = 2000;
+                scoreSide = ball.touchDown();
+                if (scoreSide == 0) return;
+                int score = (scoreSide == 1)? ++scorePlayer1:++scorePlayer2;
+                backGround.updateScore(score, scoreSide);
+                countdown = 2000;
                 gameState++;
             }
+
             case 2 -> { // countdown before resetting the positions, so the players acknowledge the score
-                if(scoreCountDown > 0) scoreCountDown--;
+                if (countdown > 0) countdown--;
                 else {
                     resetPos();
-                    serveCountDown = 3000;
                     if (winTest()) {
+                        gameState++;
+                        menu = new WonMenu(this, input, scoreSide);
+                        countdown = 1500;
+                    } else {
+                        gameState = 0;
+                        countdown = 3000;
+                    }
+                }
+            }
+
+            case 3 -> {
+                if (menu == null) {
+                    if (countdown > 0) countdown--;
+                    else {
                         gameState = -1;
                         master.openMenu();
                     }
-                    else gameState = 0;
-                }
+                } else menu.update(); //else the menu will not update
             }
         }
     }
@@ -93,12 +104,13 @@ public class MatchManager implements Renderable, Updatable, MenuMaster<MatchMana
 
     @Override
     public void render(Graphics g){
-        renderBackGround(g);
+        backGround.render(g);
         player1.render(g);
         player2.render(g);
         ball.render(g);
         if (menu != null) menu.render(g);
     }
+
     @Override
     public void menuActions(PauseMenuOptions action) {
         /*
@@ -112,7 +124,7 @@ public class MatchManager implements Renderable, Updatable, MenuMaster<MatchMana
 
     @Override
     public void openMenu() {
-        menu = new PauseMenu<PauseMenuOptions>(this, input);
+        menu = new PauseMenu<>(this, input);
     }
 
     @Override
@@ -120,9 +132,6 @@ public class MatchManager implements Renderable, Updatable, MenuMaster<MatchMana
         this.menu = null;
     }
 
-    public void renderBackGround(Graphics g) {
-        backGround.render(g);
-    }
 
     public void resetPos() {
         player1.y = player2.y = middleY - (float) Racket.HEIGHT / 2;
@@ -131,6 +140,8 @@ public class MatchManager implements Renderable, Updatable, MenuMaster<MatchMana
     }
 
     public void startGame(int gameMode) {
+        backGround.updateScore(0,1);
+        backGround.updateScore(0,-1);
         this.gameMode = gameMode;
         scorePlayer1 = scorePlayer2 = 0;
         switch (gameMode) {
@@ -148,7 +159,7 @@ public class MatchManager implements Renderable, Updatable, MenuMaster<MatchMana
             }
         }
         ball = new Ball(this, player1, player2);
-        serveCountDown = 1000;
+        countdown = 1000;
 
         player1.x = Ball.RADIUS * 3;
         player2.x = Game.WIDTH - Ball.RADIUS * 3;
@@ -162,7 +173,8 @@ public class MatchManager implements Renderable, Updatable, MenuMaster<MatchMana
             // causes a return because we shouldn't update anything else if there is a PauseMenu.
             return;
         }
-        if (input.actionJustPressed(GameActions.PAUSE)) {
+        if (input.actionActivated(GameActions.PAUSE)) {
+            //should have been actionJustPressed, but it doesn't work because of tick doubling
             openMenu();
             // same as above, it's safer to stop right away
             return;
@@ -197,10 +209,9 @@ public class MatchManager implements Renderable, Updatable, MenuMaster<MatchMana
         updateEntities();
     }
 
-    public boolean winTest() { //TODO : might need a rework later, so we actually know who won
+    public boolean winTest() {
         if (scorePlayer1 == 11 || scorePlayer2 == 11) return true;
         if (scorePlayer1 >= 9 && scorePlayer1 - scorePlayer2 > 1) return true;
-        if (scorePlayer2 >= 9 && scorePlayer2 - scorePlayer1 > 1) return true;
-        return false;
+        return scorePlayer2 >= 9 && scorePlayer2 - scorePlayer1 > 1;
     }
 }
